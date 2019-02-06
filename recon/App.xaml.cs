7592c6ca915a6ec2bd8ab1bfedd50771739f2801
Yohance McDonald <pyrocro@gmail.com>
2019-02-06 17:21:55 +0900

    Initial Commit

---
+++ recon/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using recon.Views;
+//using System.Net.WebSockets;
+using System.Threading;
+using WebSocketSharp;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace recon
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            /*var cts = new CancellationTokenSource();
+            ClientWebSocket ws = new ClientWebSocket();
+            ws.ConnectAsync(new Uri("ws://proxy.8codebubble.com:6021/api"),cts.Token);
+            System.Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            System.Console.WriteLine("reached here!");
+            System.Console.ResetColor();*/
+
+            using (var ws = new WebSocket("ws://proxy.8codebubble.com:6021/api"))
+            {
+                ws.OnMessage += (sender, e) => Console.WriteLine("here");
+                ws.Connect();
+                ws.Send();
+
+            }
+
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ recon/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using recon.Models;
+
+namespace recon.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
