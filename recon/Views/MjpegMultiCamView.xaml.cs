68cd141a6810bab3773ca2f3f74f65a845ace393
Yohance McDonald <pyrocro@gmail.com>
2019-02-13 18:18:07 +0900

    testing multicam

---
+++ recon/Views/MjpegMultiCamView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace recon.Views
+{
+    public partial class MjpegMultiCamView : ContentPage
+    {
+        public MjpegMultiCamView()
+        {
+            InitializeComponent();
+            var assembly = IntrospectionExtensions.GetTypeInfo(typeof(recon.Views.AboutPage)).Assembly;
+            Stream stream = assembly.GetManifestResourceStream("recon.Views.MjpegMultipleCam.html");
+            StreamReader reader = new StreamReader(stream);
+            String _htmlString = reader.ReadToEnd();
+
+            HtmlWebViewSource html = new HtmlWebViewSource();
+            html.Html = _htmlString;
+            mjpegViewer.Source = html;
+            mjpegViewer.HorizontalOptions = LayoutOptions.Fill;
+            mjpegViewer.VerticalOptions = LayoutOptions.Fill;
+        }
+    }
+}
